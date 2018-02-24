1d022f2dbf6fd60671035ecd98e6f1d12702b65d
Jacob Spade <spade.jacob@yahoo.com>
2018-02-23 19:43:04 -0800

    put in jquery search bar filter plugin

--- js/app.js
+++ js/app.js
@@ -1,5 +1,75 @@
-
+//lightbox plugin
 
 lightbox.option({
-  'fadeDuration': 2000
+  'alwaysShowNavOnTouchDevices': true,
+  'wrapAround': true,
+  'fadeIn': 1000,
+  'fadeOut': 1000
 });
+
+//filter picture
+
+var Shuffle = window.Shuffle;
+
+window.shuffleInstance = new Shuffle(document.querySelector('.my-grid-with-images'), {
+  itemSelector: '.js-item',
+  sizer: '.my-sizer-element',
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Lokesh Dhaker
+//http://lokeshdhakar.com/projects/lightbox2/#examples
+//The MIT License (MIT)
+
+// Copyright (c) 2015 Lokesh Dhakar
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
